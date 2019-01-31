d4c16d82fa10cddef4c8f1289c6be09acb97d2c9
Luis Gomez <luisgomez1800@gmail.com>
2019-01-30 23:27:19 -0800

    Fixed Node Dereference bug in add(), almost done with tests, saving state

--- labs/4_ordered_lists/ordered_list.py
+++ labs/4_ordered_lists/ordered_list.py
@@ -1,6 +1,6 @@
-# Luis Gomez
-# CPE 202
-
+"""
+Author: Luis Gomez
+"""
 class Node:
     """Node for use with doubly-linked list"""
     def __init__(self, item, next=None, prev=None):
@@ -17,13 +17,13 @@ class OrderedList:
         self.sentinel.next = self.sentinel
         self.sentinel.prev = self.sentinel 
 
-    def is_empty(self): # DONE
+    def is_empty(self): # DONE, TESTED
         """
         Returns back True if OrderedList is empty
         """
         return self.sentinel.next == self.sentinel and self.sentinel.prev == self.sentinel
 
-    def add(self, item): # DONE
+    def add(self, item): # DONE, TESTED
         """
         Adds an item to OrderedList, in the proper location based on ordering of items
         from lowest (at head of list) to highest (at tail of list)
@@ -37,53 +37,58 @@ class OrderedList:
             return
 
         item_list = self.python_list()
-        result = self.binary_search_add( item, 0, item_list.len()-1, item_list)
+        result = self.bin_search_add(item, 0, len(item_list)-1, item_list) # returns index tuple or None
         if result is not None:
-            if result is (int, None): # larger than Tail
-                print(100)
+            if result[0] != None and result[1] == None: # larger than Tail, (high, None)
                 self.sentinel.prev.next, current.prev = current, self.sentinel.prev
                 self.sentinel.prev = current
-            elif result is (None, int): # smaller than Head
-                print(200)
+            elif result[0] == None and result[1] != None: # smaller than Head, (None, low)
                 self.sentinel.next.prev, current.next = current, self.sentinel.next
                 self.sentinel.next = current
-            elif result is (int, int):
-                print(300)
+            elif result[0] != None and result[1] != None: # (low, high)
                 nodes = self.node_list()
                 nodes[result[0]].next, current.prev = current, nodes[result[0]]
                 nodes[result[1]].prev, current.next = current, nodes[result[1]]
         return
 
-    def remove(self, item): # DONE
+    def remove(self, item): # DONE, TESTED, FIXED DERFERENCING BUG
         """
         Removes an item from OrderedList. If item is removed (was in the list) returns True
         If item was not removed (was not in the list) returns False
         """ 
         if self.is_empty(): raise IndexError
         if item is None: raise ValueError
-        
+
         items = self.python_list() 
         item_index = self.bin_search(item, 0, len(items)-1, items)
-
-        if item_index is None: return False # 0) : item not in list
         
-        current = self.node_list()[item_index] # copy of item's Node
+        if item_index is None: return False # 0) : item not in list
+
+        current = self.node_list()[item_index]
         if len(items) == 1:                # 1) removing creates empty list
-            self.sentinel.next, self.sentinel.prev = self.sentinel, self.sentinel
+            #print("EMPTY", item, item_index)
+            self.sentinel.next = self.sentinel 
+            self.sentinel.prev = self.sentinel
             return True
         elif item_index == 0:               # 2) removing HEAD
+            #print("HEAD", item, item_index)
             self.sentinel.next = current.next
+            self.sentinel.next.prev = None
             return True
-        elif item_index == len(nodes)-1:   # 3) removing TAIL
+        elif item_index == len(items)-1:   # 3) removing TAIL
             self.sentinel.prev =  current.prev
+            self.sentinel.prev.next = None
             return True
-        else:                               # 4) removing item between HEAD and TAIL 
+        else:                               # 4) removing item between HEAD and TAIL
+            #print("BETWEEN", item, item_index)
             current.prev.next, current.next.prev =  current.next, current.prev 
             return True
 
-    def index(self, item): # DONE
-        """Returns index of an item in OrderedList (assuming head of list is index 0).
-        If item is not in list, return None"""
+    def index(self, item): # DONE, TESTED
+        """
+        Returns index of an item in OrderedList (assuming head of list is index 0).
+        If item is not in list, return None
+        """
         # is list empty?
         if self.is_empty(): raise IndexError
         if item is None: raise ValueError
@@ -91,7 +96,7 @@ class OrderedList:
         item_list = self.python_list()
         return self.bin_search(item, 0, len(item_list)-1, item_list)
 
-    def pop(self, index): # DONE
+    def pop(self, index): # DONE, TESTED
         """
         Removes and returns item at index (assuming head of list is index 0).
         If index is negative or >= size of list, raises IndexError
@@ -106,7 +111,7 @@ class OrderedList:
             return current.item
         else: raise IndexError
 
-    def search(self, item): # RECURSIVE, DONE
+    def search(self, item): # RECURSIVE, DONE, TESTED
         """
         Searches OrderedList for item, returns True if item is in list, False otherwise
         """
@@ -117,32 +122,36 @@ class OrderedList:
         else: return False
 
     def python_list(self): # DONE
-        """Return a Python list representation of OrderedList, from head to tail
-        For example, list with integers 1, 2, and 3 would return [1, 2, 3]"""
+        """
+        Return a Python list representation of OrderedList, from head to tail
+        For example, list with integers 1, 2, and 3 would return [1, 2, 3]
+        """
         # Empty list?
         if self.is_empty(): raise IndexError
         nodes = self.node_list()
         return [node.item for node in nodes]
 
-    def python_list_reversed(self): # RECURSIVE, DONE
+    def python_list_reversed(self): # RECURSIVE, DONE, TESTED
         """Return a Python list representation of OrderedList, from tail to head, using recursion
         For example, list with integers 1, 2, and 3 would return [3, 2, 1]"""
         if self.is_empty(): raise IndexError
         items = self.python_list()
         return self.reverse_rec(items)
 
-    def size(self): # RECURSIVE, DONE
+    def size(self): # RECURSIVE, DONE, TESTED
         """
         Returns number of items in the OrderedList
         """
         if self.is_empty(): raise IndexError
-        return self.count_sandwich(self.sentinel)
+        current = Node(None,self.sentinel.next, self.sentinel.prev)
+        return self.count_triangular(current)
     
     def reverse_rec(self, int_list): # HELPER FUNCTION
         """
         recursively reverses a list of numbers and returns the reversed list
         If list is None, raises ValueError
         """
+        if self.is_empty(): raise IndexError
         if int_list is None : raise ValueError # base case
         
         if len(int_list) == 1 : return int_list # base case
@@ -153,8 +162,10 @@ class OrderedList:
         return new_list
     
     def node_list(self): # HELPER FUNCTION
-        """Return a Python list representation of OrderedList, from head to tail
-        For example, list with integers 1, 2, and 3 would return [1, 2, 3]"""
+        """
+        Return a Python list representation of OrderedList, from head to tail
+        For example, list with integers 1, 2, and 3 would return [1, 2, 3]
+        """
         # Empty list?
         if self.is_empty(): raise IndexError
         node_list = [self.sentinel.next]
@@ -163,20 +174,20 @@ class OrderedList:
                 node_list.append(node.next)
         return node_list
 
-    def count_sandwich(self, sentinel, count=0): # RECURSIVE HELPER FUNCTION
+    def count_triangular(self, sentinel, count=0): # RECURSIVE HELPER FUNCTION
         """
-        Recursive function that utilizes a copy of a sentinel node to count the size of a linked list. Counts faster than starting from one end. Think sandwich... Outwards to in
+        Recursive function that utilizes a copy of a sentinel node to count the size of a linked list. Counts faster than starting from one end. Counts triangular numbers effectively and accounts for non triangular numbers too
         """
         if sentinel is None: raise ValueError
         
         current = sentinel
 
-        if current.next == self.sentinel and current.prev == self.sentinel: return count # base case
-        else: count += 1
-
-        if current.next == current.prev: return count # reached center
-        else: current.next, current.prev = current.next.next, current.prev.prev # shift inward
-        return self.count_sandwhich(current, count) # recursive call
+        if current.next == current.prev: return count+1 # reached center
+        elif current.next.next == current.prev: return (count+2) # Non-Triangular Number
+        else: 
+            current.next, current.prev, = current.next.next, current.prev.prev # Shift inwards
+            count += 2
+        return self.count_triangular(current, count) # recursive call
 
     def bin_search(self, target, low, high, item_list): # RECURSIVE HELPER FUNCTION
         """
@@ -194,11 +205,11 @@ class OrderedList:
 
         # check upper half
         if target > item_list[mid]:
-            result  = bin_search(target, mid, high, item_list)
+            result  = self.bin_search(target, mid, high, item_list)
             return result
         # check lower half
         elif target < item_list[mid]:
-            result = bin_search(target, low, mid, item_list)
+            result = self.bin_search(target, low, mid, item_list)
             return result
 
     def bin_search_add(self, target, low, high, item_list): # HELPER FUNCTION
@@ -210,18 +221,18 @@ class OrderedList:
         mid = (low + high) // 2 # determines mid index
         # BASE CASES
         if item_list[mid] == target: return None
-        if item_list[high] == target: return None
-        elif target > high: return (high, None)
-        if item_list[low] == target: return None
-        elif target < low: return (None, low)
-        if low == high-1: 
+        elif item_list[high] == target: return None
+        elif target > item_list[high]: return (high, None) # target is larger than set
+        elif item_list[low] == target: return None
+        elif target < item_list[low]: return (None, low) # target is smaller than set
+        elif low == high-1: 
             return (low, high) # return index tuple
 
         # check upper half
         if target > item_list[mid]:
-            result  = bin_search(target, mid, high, item_list)
+            result  = self.bin_search_add(target, mid, high, item_list)
             return result
         # check lower half
         elif target < item_list[mid]:
-            result = bin_search(target, low, mid, item_list)
+            result = self.bin_search_add(target, low, mid, item_list)
             return result
--- labs/4_ordered_lists/ordered_list_tests.py
+++ labs/4_ordered_lists/ordered_list_tests.py
@@ -1,3 +1,6 @@
+"""
+Author: Luis Gomez
+"""
 import unittest
 from ordered_list import *
 
@@ -6,18 +9,171 @@ class TestLab4(unittest.TestCase):
     def test_simple(self):
         t_list = OrderedList()
         t_list.add(10)
-        self.assertEquals(t_list.python_list(), [10])
-        self.assertEquals(t_list.size(), 1)
-        self.assertEquals(t_list.index(10), 0)
+        self.assertEqual(t_list.python_list(), [10])
+        self.assertEqual(t_list.size(), 1)
+        self.assertEqual(t_list.index(10), 0)
         self.assertTrue(t_list.search(10))
         self.assertFalse(t_list.is_empty())
-        self.assertEquals(t_list.python_list_reversed(), [10])
+        self.assertEqual(t_list.python_list_reversed(), [10])
         self.assertTrue(t_list.remove(10))
         self.assertTrue(t_list.is_empty())
         with self.assertRaises(IndexError):
             t_list.python_list()
         t_list.add(10)
-        self.assertEquals(t_list.pop(0), 10)
+        self.assertEqual(t_list.pop(0), 10)
+
+    def test_is_empty(self):
+        t_list = OrderedList()
+        self.assertTrue(t_list.is_empty())
+        t_list.add(3.14)
+        self.assertFalse(t_list.is_empty())
+        self.assertEqual(t_list.python_list_reversed(), [3.14])
+        self.assertTrue(t_list.remove(3.14))
+        self.assertTrue(t_list.is_empty())
+        t_list.add(5.0)
+        self.assertFalse(t_list.is_empty())
+        self.assertEqual(t_list.pop(0), 5.0)
+        self.assertTrue(t_list.is_empty())
+
+    def test_add(self):
+        t_list = OrderedList()
+        with self.assertRaises(ValueError):
+            t_list.add(None)
+        t_list.add(3.14)
+        self.assertEqual(t_list.python_list(), [3.14])
+        t_list.add(1.0)
+        self.assertEqual(t_list.python_list(), [1.0, 3.14])
+        t_list.add(2.785)
+        self.assertEqual(t_list.python_list(), [1.0, 2.785, 3.14])
+        t_list.add(2.785)
+        self.assertEqual(t_list.python_list(), [1.0, 2.785, 3.14])
+        t_list.add(3)
+        self.assertEqual(t_list.python_list(), [1.0, 2.785, 3, 3.14])
+
+    def test_remove(self):
+        t_list = OrderedList()
+        with self.assertRaises(IndexError):
+            t_list.remove(None)
+        t_list.add(99.9)
+        t_list.add(28)
+        t_list.add(55.31)
+        t_list.add(42.1)
+        self.assertEqual(t_list.python_list(), [28, 42.1, 55.31, 99.9])
+        self.assertFalse(t_list.remove(100))
+        self.assertEqual(t_list.python_list(), [28, 42.1, 55.31, 99.9])
+        self.assertTrue(t_list.remove(28))
+        self.assertEqual(t_list.python_list(), [42.1, 55.31, 99.9])
+        self.assertTrue(t_list.remove(55.31))
+        self.assertEqual(t_list.python_list(), [42.1, 99.9])
+        self.assertTrue(t_list.remove(99.9))
+        self.assertEqual(t_list.python_list(), [42.1])
+
+    def test_index(self):
+        t_list = OrderedList()
+        with self.assertRaises(IndexError):
+            t_list.index(0)
+        t_list.add(39.6)
+        with self.assertRaises(ValueError):
+            t_list.index(None)
+        t_list.add(99.9)
+        t_list.add(28)
+        t_list.add(55.31)
+        t_list.add(42.1)
+        self.assertEqual(t_list.python_list(), [28, 39.6, 42.1, 55.31, 99.9])
+        self.assertEqual(t_list.index(39.6), 1)
+        self.assertEqual(t_list.index(99.9), 4)
+        self.assertEqual(t_list.index(28), 0)
+        self.assertEqual(t_list.index(55.31), 3)
+        self.assertEqual(t_list.index(42.1), 2)
+        t_list.remove(39.6)
+        self.assertEqual(t_list.index(99.9), 3)
+        self.assertEqual(t_list.index(55.31), 2)
+        self.assertEqual(t_list.index(42.1), 1)
+
+    def test_pop(self):
+        t_list = OrderedList()
+        with self.assertRaises(IndexError):
+            t_list.pop(0)
+        t_list.add(39.6)
+        t_list.add(1)
+        t_list.add(30)
+        t_list.add(.5)
+        self.assertEqual(t_list.python_list(), [.5, 1, 30, 39.6])
+        with self.assertRaises(ValueError):
+            t_list.pop(None)
+        with self.assertRaises(IndexError):
+            t_list.pop(-1)
+        with self.assertRaises(IndexError):
+            t_list.pop(4)
+        self.assertEqual(t_list.pop(2), 30)
+        self.assertEqual(t_list.python_list(), [.5, 1, 39.6])
+        self.assertEqual(t_list.pop(0), .5)
+        self.assertEqual(t_list.python_list(), [1, 39.6])
+        self.assertEqual(t_list.pop(1), 39.6)
+        self.assertEqual(t_list.python_list(), [1])
+        self.assertEqual(t_list.pop(0), 1)
+        self.assertTrue(t_list.is_empty())
+
+    def test_search(self):
+        t_list = OrderedList()
+        with self.assertRaises(IndexError):
+            t_list.search(0)
+        t_list.add(39.6)
+        t_list.add(1)
+        t_list.add(30)
+        t_list.add(.5)
+        with self.assertRaises(ValueError):
+            t_list.search(None)
+        self.assertTrue(t_list.search(1))
+        self.assertTrue(t_list.search(.5))
+        self.assertTrue(t_list.search(39.6))
+        self.assertTrue(t_list.search(30))
+        self.assertFalse(t_list.search(30.1))
+
+    def test_python_list(self):
+        t_list = OrderedList()
+        t_list.add(99.9)
+        self.assertEqual(t_list.python_list(), [99.9])
+        t_list.add(28)
+        self.assertEqual(t_list.python_list(), [28, 99.9])
+        t_list.add(55.31)
+        self.assertEqual(t_list.python_list(), [28, 55.31, 99.9])
+        t_list.add(42.1)
+        self.assertEqual(t_list.python_list(), [28, 42.1, 55.31, 99.9])
+        self.assertFalse(t_list.remove(100))
+        self.assertEqual(t_list.python_list(), [28, 42.1, 55.31, 99.9])
+        self.assertTrue(t_list.remove(28))
+        self.assertEqual(t_list.python_list(), [42.1, 55.31, 99.9])
+        self.assertTrue(t_list.remove(55.31))
+        self.assertEqual(t_list.python_list(), [42.1, 99.9])
+        self.assertTrue(t_list.remove(99.9))
+        self.assertEqual(t_list.python_list(), [42.1])
+
+    def test_size(self):
+        t_list = OrderedList()
+        with self.assertRaises(IndexError):
+            t_list.size()
+        t_list.add(99.9)
+        self.assertEqual(t_list.size(), 1)
+        t_list.add(28)
+        self.assertEqual(t_list.size(), 2)
+        t_list.add(55.31)
+        self.assertEqual(t_list.size(), 3)
+        t_list.add(42.1)
+        self.assertEqual(t_list.size(), 4)
+        self.assertTrue(t_list.remove(28))
+        self.assertEqual(t_list.size(), 3)
+        self.assertTrue(t_list.remove(42.1))
+        self.assertEqual(t_list.size(), 2)
+        self.assertTrue(t_list.remove(55.31))
+        self.assertEqual(t_list.size(), 1)
+        self.assertTrue(t_list.remove(99.9))
+        self.assertTrue(t_list.is_empty())
+
+    def test_reverse_rec(self):
+        t_list = OrderedList()
+        with self.assertRaises(IndexError):
+            t_list.reverse_rec()
 
 if __name__ == '__main__': 
     unittest.main()
