0ef5905c8f63b3ab8f8edaabc769e00ace50c168
Gomer1800 <luisgomez1800@gmail.com>
2019-02-08 08:14:46 -0800

    first commit, huffman tree

---
+++ projects/3_huffman_tree/huffman.py
@@ -0,0 +1,51 @@
+class HuffmanNode:
+    def __init__(self, char, freq):
+        self.char = char   # stored as an integer - the ASCII character code value
+        self.freq = freq   # the frequency count associated with the node
+        self.left = None   # Huffman tree (node) to the left
+        self.right = None  # Huffman tree (node) to the right
+
+    def __lt__(self, other):
+        return comes_before(self, other) # Allows use of Python List sorting
+
+    def set_left(self, node):
+        self.left = node
+
+    def set_right(self, node):
+        self.right = node
+
+def comes_before(a, b):
+    """Returns True if node a comes before node b, False otherwise"""
+
+def combine(a, b):
+    """Creates and returns a new Huffman node with children a and b, with the "lesser node" on the left
+    The new node's frequency value will be the sum of the a and b frequencies
+    The new node's char value will be the lesser of the a and b char ASCII values"""
+
+def cnt_freq(filename):
+    """Opens a text file with a given file name (passed as a string) and counts the 
+    frequency of occurrences of all the characters within that file
+    Returns a Python List with 256 entries - counts are initialized to zero.
+    The ASCII value of the characters are used to index into this list for the frequency counts"""
+
+def create_huff_tree(freq_list):
+    """Input is the list of frequencies (provided by cnt_freq()).
+    Create a Huffman tree for characters with non-zero frequency
+    Returns the root node of the Huffman tree. Returns None if all counts are zero."""
+
+def create_code(node):
+    """Returns an array (Python list) of Huffman codes. For each character, use the integer ASCII representation 
+    as the index into the arrary, with the resulting Huffman code for that character stored at that location.
+    Characters that are unused should have an empty string at that location"""
+
+def create_header(freq_list):
+    """Input is the list of frequencies (provided by cnt_freq()).
+    Creates and returns a header for the output file
+    Example: For the frequency list asscoaied with "aaabbbbcc, would return “97 3 98 4 99 2” """
+
+def huffman_encode(in_file, out_file):
+    """Takes inout file name and output file name as parameters
+    Uses the Huffman coding process on the text from the input file and writes encoded text to output file
+    Take not of special cases - empty file and file with only one unique character"""
+
+
