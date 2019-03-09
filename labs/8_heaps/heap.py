c5aa7ea6b7290e979055d31fc8443a41826d5fc9
Luis Gomez <luisgomez1800@gmail.com>
2019-03-08 23:08:07 -0800

    all test cases passed, heap sort works!

--- labs/8_heaps/heap.py
+++ labs/8_heaps/heap.py
@@ -1,8 +1,10 @@
 """
 Luis Gomez
-Python implementation of the Binary Heap ADT, using the MaxHeap approach.
-Largest key value is always at front.
+
+Python implementation of the Binary Heap ADT, using the MaxHeap approach. Largest key value is always at front.
 This heap contains integers that represent both the priority and name of the object
+
+The Bubbling down step of the Bottom Up approach consists of applying a percdown, starting at the last parent through the first
 """
 class MaxHeap:
 
@@ -13,13 +15,17 @@ class MaxHeap:
         self.num_items = 0
 
     def enqueue(self, item):
-        """inserts "item" into the heap, returns true if successful, false if there is no room in the heap"""
+        """inserts "item" into the heap, returns true if successful, false if there is no room in the heap
+        
+        -Append new item to heap list
+        -PercUp the new item
+        """
         if item is None: raise ValueError
         if self.is_full(): return False
         self.num_items+=1
         self.heap_list.insert(self.num_items, item)
         self.perc_up(self.num_items)
-        print("End enqueue: ",self.heap_list)
+        #print("End enqueue: ",self.heap_list)
         return True
 
     def peek(self):
@@ -29,11 +35,17 @@ class MaxHeap:
 
     def dequeue(self):
         """returns max and removes it from the heap and restores the heap property
-           returns None if the heap is empty"""
+           returns None if the heap is empty
+        
+        -dequeue top of heap tree (this is the max val)
+        -insert last child into top of heap tree
+        -PercDown the top of tree
+        """
         if self.is_empty(): return None
         max, self.heap_list[1] = self.heap_list[1], self.heap_list.pop(self.num_items)
         self.num_items-=1
-        self.perc_down(1)
+        #print("deq num items :", self.num_items)
+        if self.num_items > 0: self.perc_down(1)
         return max
 
     def contents(self):
@@ -52,19 +64,21 @@ class MaxHeap:
         if self.heap_size-1 < len(alist):
             self.heap_list = [None].extend(alist)
             self.heap_size = len(self.heap_list)
-            print("Case small: ",self.heap_list)
+            #print("Case small: ",self.heap_list)
         # Case, current heap is large enough
         else:
             alist.insert(0,None)
             # print(alist)
             alist.extend([None]*(self.heap_size-len(alist))) 
             self.heap_list = alist
-            print("Case big: ",self.heap_list)
+            #print("Case big: ",self.heap_list)
         i = self.num_items//2
+        # Restore Heap Order Property
+        # Starting at final parent through first parent, PercDown
         while i >= 1:
             self.perc_down(i)
             i-=1
-        print("End Build: ",self.heap_list)
+        #print("End Build: ",self.heap_list)
 
     def is_empty(self):
         """returns True if the heap is empty, false otherwise"""
@@ -88,17 +102,17 @@ class MaxHeap:
     def perc_down(self, i):
         """where the parameter i is an index in the heap and perc_down moves the element stored
         at that location to its proper place in the heap rearranging elements as it goes."""
-        if i is None: raise ValueError  # edge case, invalid index
+        if i is None: raise ValueError              # edge case, invalid index
         if i <= 0: raise ValueError
         if i > self.num_items: raise ValueError
-        if i == self.num_items: return               # edge case, index is top of tree
+        if i == self.num_items: return               # edge case, index is last item
         left = 0
         right = 0
         # starting right most child
         child = self.find_child( i )
         # iterate through child nodes
         while child >= self.num_items//2:
-            print("perc down: ", child, self.contents())
+            # print("perc down: ", child, self.contents())
             left, right = 0, 0
             # Case, even child
             if child % 2 == 0:
@@ -125,19 +139,20 @@ class MaxHeap:
                 new_child = self.find_child( child )
                 i, child = child, new_child
                 continue
+            # Case, no child is greater
             break
 
     def find_child(self,i):
-        print("find i = ",i)
+        #print("find i = ",i)
         child = 0
         try:
             child = self.heap_list.index(self.contents()[i*2])
-            print("try " , child)
+            #print("try " , child)
         except Exception:
             child = self.heap_list.index(self.contents()[i*2-1])
-            print("except " ,child)
+            #print("except " ,child)
         finally:
-            print("finally " ,child)
+            #print("finally " ,child)
             return child
 
     def perc_up(self, i):
@@ -150,7 +165,7 @@ class MaxHeap:
         parent = i//2
         # iterate through parent nodes
         while parent > 0:
-            print("perc up: ", parent, self.contents())
+            #print("perc up: ", parent, self.contents())
             # current value > parent?
             if self.heap_list[i] > self.heap_list[parent]:
                 # swap positions if true
@@ -164,5 +179,10 @@ class MaxHeap:
         """perform heap sort on input alist in ascending order
         This method will discard the current contents of the heap, build a new heap using
         the items in alist, and mutate alist to put the items in ascending order"""
-
-
+        size = len(alist)
+        self.build_heap(alist[0:len(alist)])
+        #print("alist",alist)
+        for i in range(size):
+            removed = self.dequeue()
+            alist[-1-i] = removed
+        #print("sorted",self.heap_list[-1-size: -1])
